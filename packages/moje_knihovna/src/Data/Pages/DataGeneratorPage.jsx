694d9384e4a9312eb526bb6b0178a55be2b496d7
matejtresnak <mates1354@gmail.com>
2025-04-21 09:07:56 +0200

    upravy data generator page

--- packages/moje_knihovna/src/Data/Pages/DataGeneratorPage.jsx
+++ packages/moje_knihovna/src/Data/Pages/DataGeneratorPage.jsx
@@ -1,8 +1,32 @@
-export const DataGeneratorPage = () => {    
+import { createAsyncGraphQLAction, useAsyncAction } from "@hrbolek/uoisfrontend-gql-shared"
+
+const InsertStateMachineAsyncAction = createAsyncGraphQLAction(
+    `mutation statemachineinsert($name: String!) {
+  statemachineInsert(statemachine: {name: $name}) {
+    __typename
+    ...on InsertError {
+      failed
+      msg
+      input
+    }
+  	...StateMachineMedium
+  }
+}
+
+fragment StateMachineMedium on StateMachineGQLModel {
+  id
+  name
+}`);
+
+export const DataGeneratorPage = () => {
+    const {loading, error, entity, fetch} = useAsyncAction(InsertStateMachineAsyncAction, {name: "test"}, {deferred: true})
     return (
         <div>
-            <h1>Data Generator</h1>
-            <p>This page is under construction.</p>
+            Tlacitko<br/>
+            <button onClick={() => fetch({name: "ahojky"})}>Insert</button><br/>
+            {loading && <div>Loading...</div>}
+            {error && <div>Error: {error.message}</div>}
+            {entity && <div>Inserted: {JSON.stringify(entity)}</div>}
         </div>
     )
 }
